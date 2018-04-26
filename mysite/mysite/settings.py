1fa66b89d0b1e5419714149ea6291b72d9a5be8d
jsimon <jsimon@jsimon.oficina.apsl.net>
2018-04-26 11:48:54 +0200

    debugtoolbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,12 +26,14 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'debug_toolbar',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,4 +169,25 @@ CKEDITOR_CONFIGS = {
         'height': '600px',
         'toolbarCanCollapse': False,
     },
+}
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+def show_toolbar(request):
+    return True
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
 }
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-]
+    ]
